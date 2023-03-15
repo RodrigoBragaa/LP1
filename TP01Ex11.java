3fc78283e63297bd8fb6af8b1fadcdd9e59daa12
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-03-15 20:02:34 -0300

    Update TP01Ex11.java

--- TP01Ex11.java
+++ TP01Ex11.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/**
+ * Nomes: Rodrigo Braga e João Vitor Pedral
+ * 11. A partir do diâmetro de um círculo que será digitado, calcular e exibir sua área.
+ */
 public class TP01Ex11 {
 
 	public static void main(String[] args) {
