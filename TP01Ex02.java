f8780b60f17ddebacd45641be7d672d9ac279529
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-03-15 19:51:15 -0300

    Update TP01Ex02.java

--- TP01Ex02.java
+++ TP01Ex02.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/**
+ * @author Rodrigo Braga e João Vitor Pedral
+ * 2. Calcular e exibir a área de um quadrado, a partir do valor de sua aresta que será digitado.
+ */
 public class TP01Ex02 {
 
 	public static void main(String[] args) {
