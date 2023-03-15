2a7da36a280af7e99d72bef0601450d37799aca7
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-03-15 20:13:07 -0300

    Update TP01Ex03.java

--- TP01Ex03.java
+++ TP01Ex03.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 /**
- * @author Rodrigo Braga e João Vitor Pedral
+ * Nomes: Rodrigo Braga e João Vitor Pedral
  * 3. Calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será digitado.
  */
 public class TP01Ex03 {
