a504689568d95dfb67c94a3868f0c6f34d593828
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-03-15 20:11:36 -0300

    Update TP01Ex18.java

--- TP01Ex18.java
+++ TP01Ex18.java
@@ -1,6 +1,12 @@
 
 import java.util.Scanner;
 
+/**
+ * Nomes: Rodrigo Braga e João Vitor Pedral
+ * 18. Entrar via teclado com o valor de cinco produtos. Após as entradas, 
+ * digitar um valor referente ao pagamento da somatória destes valores. 
+ * Calcular e exibir o troco que deverá ser devolvido.
+*/
 
 public class TP01Ex18 {
     
