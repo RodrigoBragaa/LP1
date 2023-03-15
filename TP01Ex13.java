800a7258c175a6629adda14b718902b2c5c9da75
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-03-15 20:05:27 -0300

    Update TP01Ex13.java

--- TP01Ex13.java
+++ TP01Ex13.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+/**
+ * Nomes: Rodrigo Braga e João Vitor Pedral
+ * 13. Calcular e exibir a velocidade final (em km/h) de um automóvel, a 
+ * partir dos valores da velocidade inicial (em m/s), da aceleração (m/s2) e do 
+ * tempo de percurso (em s) que serão digitados.
+ */
 public class TP01Ex13 {
 
 	public static void main(String[] args) {
