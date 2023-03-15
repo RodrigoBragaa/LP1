1503934adb6ef42162742ee09fda3c61107b9d58
Rodrigo Braga Guimarães Brito <87531169+RodrigoBragaa@users.noreply.github.com>
2023-03-15 20:07:59 -0300

    Update TP01Ex15.java

--- TP01Ex15.java
+++ TP01Ex15.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/**
+ * Nomes: Rodrigo Braga e João Vitor Pedral
+ * 15. Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de dólares.
+ * Calcular e exibir o valor correspondente em Reais (R$).
+ */
+ 
 public class TP01Ex15 {
 
 	public static void main(String[] args) {
